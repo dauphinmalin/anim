9f59f7ce95cca938ed352e7b6f2b860b374f2290
enea.de-bollivier@phelma.grenoble-inp.fr <enea.de-bollivier@phelma.grenoble-inp.fr>
2018-05-14 19:36:44 +0900

    r

--- 1assignement/Particle.java
+++ 1assignement/Particle.java
@@ -3,20 +3,22 @@ public class Particle{
   private double m;//not equal to 0!!!
   private double[3] pos;//X,Y,Z
   private double[3] previouspos;
+  private double[3] vel;
   private double[3] f;//force on X,force Y,force Z
   private double dt;
 
 
-public void Particle(double m,x,y,z,fx,fy,fz,dt){
+public void Particle(double m,pos[],vel[],f[],dt){
   if(m>0){
   this.m=m;
-  this.pos[0]=pos;
-  this.pos[1]=y;
-  this.pos[2]=z;
-  this.f[0]=fx;
-  this.f[1]=fy;
-  this.f[2]=fz;
-  this.dt=dt;
+
+    this.pos=pos;
+    for(int i=0;i<3;i++){
+    this.previouspos[i]=x[i]-vel[i]*dt;
+    }
+    this.vel=vel
+    this.f=f;
+
 }
 else throw "masse equal to 0!!"
 }
