11589d49d856e8ef1d714d085b8017111d5d53f4
WANG Yinan <yinan@WANGdeMacBook-Pro.local>
2018-05-14 19:36:30 +0900

    g

--- 1assignement/Task1.java
+++ 1assignement/Task1.java
@@ -1,10 +1,22 @@
+import Particle.java
+import viewing.java
+
 class Task1{
 	public static void main (String args[]){
+		//Init
+		Particle p = new Particle[];
+		p.Particle();
+
 		while(1){
 
 		}
-		
+
 	}
+
+	public void init(){
+
+	}
+
 
 
 }
