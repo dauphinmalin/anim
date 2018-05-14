5ef67aac107cc0832397a9df459d0a8e1e1439a9
enea.de-bollivier@phelma.grenoble-inp.fr <enea.de-bollivier@phelma.grenoble-inp.fr>
2018-05-14 18:19:59 +0900

    first commit

---
+++ 1assignement/Particle.java
@@ -0,0 +1,54 @@
+class Particle{
+
+  private double m;//not equal to 0!!!
+  private double[3] pos;//X,Y,Z
+  private double[3] previouspos;
+  private double[3] f;//force on X,force Y,force Z
+  private double dt;
+
+
+public void Particle(double m,x,y,z,fx,fy,fz,dt){
+  if(m>0){
+  this.m=m;
+  this.pos[0]=pos;
+  this.pos[1]=y;
+  this.pos[2]=z;
+  this.f[0]=fx;
+  this.f[1]=fy;
+  this.f[2]=fz;
+  this.dt=dt;
+}
+else throw "masse equal to 0!!"
+}
+
+public double getX(){
+  return this.pos[0];
+}
+
+public double getY(){
+  return this.pos[1];
+}
+
+public double getZ(){
+  return this.pos[2];
+}
+
+public void setF(fx,fy,fz){
+  this.f[0]=fx;
+  this.f[1]=fy;
+  this.f[2]=fz;
+
+}
+
+public void calculatePos(){
+  double x[3];
+  x=this.pos;
+  for(i=0;i<3;i++){
+    this.pos[i]=2*this.pos[i]-this.previouspos[i]+this.dt*this.dt*this.f[i]/this.m;//Verlet for x,y,z
+
+  }
+  this.previouspos=x;
+
+}
+
+}
---
+++ 1assignement/viewing.java
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <GLFW\glfw3.h>
+
+int main(){
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+}
