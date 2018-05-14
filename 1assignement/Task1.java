7741215a0262e413bf2564b111bff22f442a179f
WANG Yinan <yinan@WANGdeMacBook-Pro.local>
2018-05-14 20:12:04 +0900

    g

--- 1assignement/Task1.java
+++ 1assignement/Task1.java
@@ -1,19 +1,36 @@
 import Particle.java
-import viewing.java
+import Viewing.java
 
 class Task1{
 	public static void main (String args[]){
-		//Init
-		Particle p = new Particle[];
-		p.Particle();
 
-		while(1){
+		//Init for the particle
+		Particle p = new Particle[];
+
+		double m = 1;
+		double dt = 0.1;
+		double[3] pos = {1,1,1};
+		double[3] vel = {1,1,1};
+		double[3] f = {0,0,-9.8};
+
+		p.Particle(m, pos[], vel[], f[], dt);
+
+		//Init for the viewer
+		Viewing viewer = new Viewing;
+
+
+		while(true){
+
+			//Draw the particle
+			viewer.drawParticles();
+
+			//Calculate the next position of the particle
+			p.calculatePos();
+
+			//Time descretization for dt second
+			Thread.sleep((int)(1000*dt);
 
 		}
-
-	}
-
-	public void init(){
 
 	}
 
