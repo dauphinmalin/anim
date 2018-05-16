dcfe6f9ce1f846e33e27e05f0a2342a5b6f67961
enea.de-bollivier@phelma.grenoble-inp.fr <enea.de-bollivier@phelma.grenoble-inp.fr>
2018-05-16 14:45:00 +0900

    recursive

--- 1assignement/Task4/Collision.java
+++ 1assignement/Task4/Collision.java
@@ -2,19 +2,33 @@
 import java.util.*;
 import java.lang.Math;
 
+class Cluster{
+  public int[] elements;
+  public double xMin;
+  public double zMin;
+  public double xMax;
+  public double zMax;
+
+Public Cluster(int[] elements,xmin,zmin,xmax,zmax){
+  this.elements=elements;
+  this.xMin=xmin;
+  this.xMax=xmax;
+  this.zMin=zmin;
+  this.zMax=zmax;
+}
+
+}
 
 class Tree{
-  private double x;
-  private double z;
-  private int[] inside;
+  Cluster cluster;
   private Tree right;
   private Tree left;
   static Particle[] particles=null;
 
 
 
-public Tree(int[] inside,double x,double z){
-  this.inside=inside;
+public Tree(Cluster cluster){
+  this.cluster=cluster;
   this.x=x;
   this.z=z;
   this.left=null;
@@ -37,32 +51,7 @@ public setParticles(Particle[] particles){
   this.particles=particles;
 }
 
-private void Recursion(){
-  int[][] result=this.cluster(inside);
-  
-
-
-
-
-
-
-}
-}
-
-public class Collision{
-  Particle[] particles;
-  Tree collisionTree;
-
-
-public Collision(Particle[] particles,double sizex,double sizez){
-  this.particles=particles;
-  int[] tab=new int[particles.length];
-  for(int i=0;i<particles.length;i++){
-    tab[i]=i;
-  }
-  this.collisionTree=new Tree(tab,sizex,sizez);
-}
-public int[][] Cluster(int[] tab){
+public Cluster[] Clustering(int[] tab){
     ArrayList<Integer> a=new ArrayList<Integer>();
     ArrayList<Integer> b=new ArrayList<Integer>();
     ArrayList<Integer> a1=new ArrayList<Integer>();
@@ -76,10 +65,19 @@ public int[][] Cluster(int[] tab){
     double prevMedz1=0;
     double prevMedx2=0;
     double prevMedz2=0;
+    double axmin=1000000000000000000000;
+    double bxmin=1000000000000000000000;
+    double azmin=1000000000000000000000;
+    double bzmin=1000000000000000000000;
+    double axmax=0;
+    double azmax=0;
+    double azmax=0;
+    double bzmax=0;
     for(i=0;i<tab.length/2;i++){
       a.add(tab[i]);
       medx1=this.particles[tab[i]].getX()+medx1;
       medz1=this.particles[tab[i]].getZ()+medz1;
+      if
 
 
     }
@@ -113,6 +111,16 @@ public int[][] Cluster(int[] tab){
       prevMedz1=medz1;
       prevMedx2=medx2;
       prevMedz2=medz2;
+      axmin=1000000000000000;
+      bxmin=1000000000000000;
+      azmin=1000000000000000;
+      bzmin=1000000000000000;
+      axmax=0;
+      azmax=0;
+      azmax=0;
+      bzmax=0;
+
+
       System.out.println("prevmedx1: "+prevMedx1+"     prevmedz1: "+prevMedz1);
       System.out.println("prevmedx2: "+prevMedx2+"     prevmedz2: "+prevMedz2);
       medx1=0;
@@ -130,6 +138,10 @@ public int[][] Cluster(int[] tab){
           medx1+=x;
           medz1+=z;
           k1+=1;
+          if{x<axmin}{axmin=x;}
+          else if {x>axmax}{axmax=x}
+          if{x<azmin}{azmin=z;}
+          else if {z>azmax}{azmax=z}
 
 
 
@@ -139,6 +151,10 @@ public int[][] Cluster(int[] tab){
           medx2+=x;
           medz2+=z;
           k2+=1;
+          if{x<bxmin}{bxmin=x;}
+          else if {x>bxmax}{bxmax=x}
+          if{x<bzmin}{bzmin=z;}
+          else if {z>bzmax}{bzmax=z}
         }
       }
       medx1=medx1/k1;
@@ -159,8 +175,9 @@ public int[][] Cluster(int[] tab){
         right[i] = iteratorb.next().intValue();
     }
 
-
-    int[][] result={left,right};
+    Cluster cleft=new Cluster(left,axmin,axmax,azmin,azmax);
+    Cluster cleft=new Cluster(left,bxmin,bxmax,bzmin,bzmax);
+    Cluster[] result={left,right};
 
 
 
@@ -168,7 +185,53 @@ public int[][] Cluster(int[] tab){
 }
 
 
+
+public void Recursion(){
+  if (this.cluster.elements.length>1){
+
+
+  Cluster[] result=this.cluster(this.inside);
+  this.left=new Tree(Cluster[0]);
+  this.right=new Tree(Cluster[1]);
+  this.left.Recursion();
+  this.right.Recursion();
+  }
+
+
+
+
+
+
+
+
+
+}
+}
+
+public class Collision{
+  Particle[] particles;
+  Tree collisionTree;
+
+
+public Collision(Particle[] particles,double sizex,double sizez){
+  this.particles=particles;
+  int[] tab=new int[particles.length];
+  for(int i=0;i<particles.length;i++){
+    tab[i]=i;
+  }
+  this.collisionTree=new Tree(tab,sizex,sizez);
+}
+
+
+
 public void checkColision(){
+  int[] inside=new int[this.particles.length];
+  for(int i=0;i<this.particles.length;i++){
+    inside[i]=i;
+  }
+  Cluster cluster=new Cluster(inside,0,0,this.sizex,this.sizez);
+  Tree tree= new Tree(cluster);
+  tree.Collision();
 
 }
 }
