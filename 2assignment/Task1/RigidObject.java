68871eaa198f12e3368634735faa14bd262f4113
WANG Yinan <yinan@WANGdeMacBook-Pro.local>
2018-06-17 12:38:00 +0900

    1

--- 2assignment/Task1/RigidObject.java
+++ 2assignment/Task1/RigidObject.java
@@ -2,59 +2,32 @@ class RigidObject extends PrimitiveObject{
 
   private double[] constraintleft; //constraint on position and rotation for the left son;
   private double[] constraintright;   //constraint on position and angle for right son;
-  private ObjectTree TreeofElements;
-  private PrimitiveObject[] elements;
-  private constraints[][];
-  public void Draw(){
-    for(int i=0;i<objects.length;i++){
-      elements[i].Draw();
-    }
+  private PrimitiveObject left;
+  private PrimitiveObject right;
+public void Draw(){
+  left.Draw();
+  right.Draw();
+}
+
+public void setPos(double[] position,double rotation){
+  super(position,rotation);
+  double[] leftpos={0,0,0};
+  for(i=0;i<3;i++){
+    leftpos[i]=position[i]+
   }
-  public RigidObject(PrimitiveObject[] objects,double[] vel,double[] rotation,double[] f,double dt){
-    super(0,pos,vel,rotation,f,dt,0,0);
-    this.elements=objects.clone();
-    int i=0;
-    for(int i=0;i<this.elements.length){
-      for(int j=0;j<this.elements[i].pos.lenght;j++){
-        this.pos[j]+=(this.elements[i].m*this.elements[i].pos[j]+this.pos[j]*this.m)/(this.m+this.elements[i].m);
-        this.m=this.m+this.elements[i].m;
-      }
-    }
-    this.calculateBoundingVolume();
-    this.TreeofElements=new ObjectTree(this.particles,min,max,m,cm);
-
-  }
-
-  public void Draw(){
-    for(int i=1;i<this.elements.length;i++){
-      elements[i].Draw();
-    }
-  }
-
-  public void calculateBoundingVolume(){
-    for(int j=0;j<this.elements[i].lenght;j++){
-      this.extremeInf[j]=this.elements[0].extremeInf[j];
-      this.extremeSup[j]=this.elements[0].extremeSup[j];
-    }
-    for(int i=1;i<this.elements.length;i++){
-      for(int j=0;j<this.elements[i].lenght;j++){
-        if(this.elements[i].extremeInf[j]<this.extremeInf[j]){
-          this.extremeInf[j]=this.elements[i].extremeInf[j];
-        }
-        if(this.elements[i].extremeSup[j]>this.extremeSup[j]){
-          this.extremeSup[j]=this.elements[i].extremeSup[j];
-        }
-      }
-    }
-  }
+  this.left.setPos();
+  this.right.calculatePos();
+}
 
 
 
+public RigidObject(PrimitiveObject[] objects,double[] pos,double[] vel,double[] rotation,double[] f,double dt){
+  super(0,pos,vel,rotation,f,dt,0,0);
 
+}
 
+private void CreateTree();
 
-  private void CreateTree();
-
-  public void calculatePosition();
+public void calculatePosition();
 
 }
